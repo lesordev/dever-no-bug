e7ff9a9f1abdacd191b8cab4fb596e898a3e6c58
luc.le <trungluc.dev@gmail.com>
2023-02-23 01:04:22 +0700

    init api application

--- app/admin/postcss.config.js
+++ app/admin/postcss.config.js
@@ -2,7 +2,9 @@ const { join } = require('path');
 
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      config: join(__dirname, 'tailwind.config.js'),
+    },
     autoprefixer: {},
   },
 };
--- app/admin/tailwind.config.js
+++ app/admin/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['app/admin/**/*.{js,ts,jsx,tsx}'],
+  content: ['app/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
